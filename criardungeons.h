24c1557175bcc8cc9f9eeb6c893ddb95a5ca970d
HagiSL <leonelhagig@gmail.com>
2025-12-05 01:57:23 -0300

    Gerador de Dungeon + Mudanças Struct

--- criardungeons.h
+++ criardungeons.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
 #include "structs.h"
-void criardugeon(MODELO_SALA_DUNGEON mapa[6][6], int dificuldade, int tipodungeon);
-void selecionadungeon(int tipodungeon);
+void CriarDungeon(MODELO_SALA_DUNGEON mapa[6][6], int dificuldade, int tipodungeon);
+void SelecionaDungeon(int tipodungeon);
+void CriaSalaBoss(MODELO_SALA_DUNGEON mapa[6][6]);
