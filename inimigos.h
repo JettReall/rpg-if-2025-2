326650d7d468153c1f5dbe326e5039daed02c75e
HagiSL <leonelhagig@gmail.com>
2025-12-05 00:47:45 -0300

    Atualizações

--- inimigos.h
+++ inimigos.h
@@ -3,8 +3,13 @@
 #include "structs.h"
 
 // Declara o array global
-extern PERSONAGEM inimigo[22];
+extern INIMIGOS inimigo[5];
 // Declara a função que preenche os inimigos
-void criarinimigos();
+void criar_ini_vento();
+void criar_ini_bosque();
+void criar_ini_agua();
+void criar_ini_fogo();
+void criar_ini_final();
+int max_inimigo = 4; // Nao contabiliza o boss.
 
 #endif
