82200575fed290017f2d437f18763a9437036c25
JettReall <guilherme.felizardo@icloud.com>
2025-12-11 20:58:22 -0300

    Delete varglboal.h

--- varglboal.h
+++
@@ -1,4 +0,0 @@
-#include "structs.h"
-#define max_inimigo_Dungeon 4  // Nao contabiliza o boss.
-#define max_inimigo_sala 4 // Quantia max de inimigo que pode ter por sala.
-#define max_array_inimigo 20
