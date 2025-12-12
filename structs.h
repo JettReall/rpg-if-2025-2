13f2d39a2cca426092bf74e46f09c1d0d1a9c53f
JettReall <guilherme.felizardo@icloud.com>
2025-12-11 21:04:45 -0300

    Create structs.h

---
+++ structs.h
@@ -0,0 +1,145 @@
+#ifndef STRUCTS_H     // Se STRUCTS_H não está definido
+#define STRUCTS_H 
+
+#include "varglobal.h"
+//enuns:
+typedef enum {
+    HPMAX,
+    DEFFIS,
+    DEFMAG,
+    SPEED,
+    ATQFIS,
+    ATQMAG,
+    NIVEL
+}Stats;
+
+enum Coordenadas {
+    X,
+    Y,
+};
+
+
+typedef enum{
+    ATAQUEFIS,
+    ATAQUEMAG,
+    CURA,
+}Tipo;
+
+typedef enum {
+    DANO,
+    STATMOD
+}Efeito_Tipos;
+
+typedef enum {
+    NADA,
+    BAU,
+    ALAVANCA,
+    SALA_BOSS,
+    ENTRADA,
+    OBSTACULO, //Ele nunca entrará numa sala do tipo obstáculo
+    SEM_MOB,
+}Especiais;
+
+typedef enum {
+    INVALIDO,
+    VALIDO
+}Valido;
+
+typedef enum {
+    VENTO = 1,
+    BOSQUE,
+    AGUA,
+    FOGO,
+}Dungeon;
+
+typedef enum {
+    BOSS,
+    FACIL,
+    MEDIO,
+    DIFICIL
+}Dificuldade;
+
+typedef enum {
+    NORTE,
+    LESTE,
+    SUL, 
+    OESTE,
+} DIRECOES;
+
+typedef enum {
+    CARREGAR,
+    JOGAR,
+    DIFICULDADE
+}MenuOp;
+
+typedef enum {
+    GUERREIRO,
+    MAGO,
+    SACERDOTE
+}MenuClasses;
+//-------------------------------------------------------------------------------------------------------------------
+//structs:
+
+typedef struct {
+    char Nome[50];
+    char Descricao[MAX_TAM_DESCRICAO];
+    int Stat[7]; //status
+    int Custo; //dinheiro
+}ITEM;
+
+typedef struct{
+    char Nome[50];
+    char Descricao[MAX_TAM_DESCRICAO];
+    int Tipo,Valor;
+}CARTA;
+
+typedef struct {  //efeitos overtime ex: bleed, poison, buff de dano, buff de def etc
+    char Nome[50];
+    int Tipo,Valor,Stat,Contador;
+}EFEITO;
+
+typedef struct NO_BARALHO{ //baralho eh uma lista encadeada
+    CARTA Carta;    //carta eh um struct carta
+    struct NO_BARALHO *PtrProx;
+}NO_BARALHO;
+
+typedef struct{
+    int UltimoSavepoint;
+    CARTA Cartas[100];
+    int Dificuldade; 
+    int Dinheiro;
+}SAVE_DADOS;
+
+typedef struct {
+    char Nome[50];//nome
+    int Stat[7];//stats max
+    int HpAtual,Xp;//hp atual e xp atual
+    NO_BARALHO *Baralho; //baralho lista encadeada
+}PERSONAGEM;
+
+typedef struct{
+    char Nome[50];
+    ITEM Inventario[20];
+}NPC;
+
+typedef struct{
+    char Nome[30];
+    int Tipo, Valor;
+}SKILL_INI;
+
+typedef struct{
+    char Nome[50];//nome
+    int Stat[7];//stats max
+    int HpAtual,Xp;//hp atual e xp atual
+    SKILL_INI Habilidades[2];
+}INIMIGOS;
+
+typedef struct{
+    char DescricaoSala[MAX_TAM_DESCRICAO];
+    char Direcoes[4][30]; //Norte, Leste, Sul, Oeste
+    int Tipo_Especial; //Especial = 0, não é pra voltar nada e outros valores podem ser algo como: 1 = bau, 2 = alavanca...
+    int Lugar_Valido; // 0 = Não é possivel Acessar (De forma alguma), 1 = É possivel de ser acessado.
+    INIMIGOS inimigos[4];//inimigos presentes no quarto
+}MODELO_SALA_DUNGEON;
+
+#endif
